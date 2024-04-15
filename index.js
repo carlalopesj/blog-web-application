e81347c1af5b1cfcbc944e205e31b4a3b5d1f31f
Carla Lopes Jovito <carlaljovito@gmail.com>
2024-04-14 21:59:12 -0300

    Header and Footer

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+});
